965dcd6ffc2705f203927dc20badd8eba55d17b0
Wasim <jaffershaikwasim@gmail.com>
2026-07-20 19:19:45 +0530

    added a badge to the hero page

--- src/components/tawfiq/Hero.jsx
+++ src/components/tawfiq/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import PrayerClock from "./PrayerClock";
+// import PrayerClock from "./PrayerClock";
 import ComingSoon from "./ComingSoon";
 
 export default function Hero() {
